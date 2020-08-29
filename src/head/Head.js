54cf50e9518c5c6c4c1f2b34bf1b4be33a3fde67
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-08-29 13:00:32 -0400

    image added to header

--- src/head/Head.js
+++ src/head/Head.js
@@ -1,9 +1,15 @@
 import React from "react";
+import tempbee from "../images/tempbee.png";
 
 export default function Head() {
   return (
     <header className="head">
       <h1>Karmafleet University</h1>
+      <div className="logo">
+        <img src={tempbee} alt="Logo" />
+      </div>
+      <p>EVE is hard, but it does not have to be.</p>
+      <p>Let KFU show you "DA WAE"</p>
     </header>
   );
 }
