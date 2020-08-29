7607315f6d284a6e82e7cefe15f7d866e8afe41a
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-08-29 12:04:58 -0400

    header created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import "./sassMaster/index.scss";
+
+import Head from "./head/Head";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Head />
+    </div>
+  );
 }
 
 export default App;
--- src/head/Head.js
+++ src/head/Head.js
@@ -1,1 +1,9 @@
 import React from "react";
+
+export default function Head() {
+  return (
+    <header className="head">
+      <h1>Karmafleet University</h1>
+    </header>
+  );
+}
