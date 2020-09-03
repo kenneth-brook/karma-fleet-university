7a5a07573c026b793b090127698f56168d2bb286
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-09-02 20:50:56 -0400

    replaced bee

--- src/head/Head.js
+++ src/head/Head.js
@@ -1,12 +1,12 @@
 import React from "react";
-import tempbee from "../images/tempbee.png";
+import KFUBee from "../images/KFU-Bee.gif";
 
 export default function Head() {
   return (
     <header className="head">
       <h1>Karmafleet University</h1>
       <div className="logo">
-        <img src={tempbee} alt="Logo" />
+        <img src={KFUBee} alt="Logo" />
       </div>
       <p>EVE is hard, but it does not have to be.</p>
       <p>Let KFU show you "DA WAE"</p>
