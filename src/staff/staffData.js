af3fb4cd90277e9e01003a3a8e821b145ce7a1d8
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-09-24 20:23:59 -0400

    file cleanup

--- src/staff/staffData.js
+++ src/staff/staffData.js
@@ -20,31 +20,3 @@ const staffData = [
 ];
 
 export default staffData;
-
-// const staffCard = document.querySelector(".c .back");
-// staffData.forEach((item1) => {
-//   back.appendChild(createBack(item1.name, item1.img, item1.role));
-// });
-
-// function createBack(name, img, role) {
-//   const cardBack = document.createElement("div");
-//   cardBack.classList.add("cardBack");
-
-//   const staffName = document.createElement("h1");
-
-//   const staffImg = document.createElement("img");
-
-//   const staffRole = document.createElement("h2");
-
-//   //append stack
-//   cardBack.appendChild(staffName);
-//   cardBack.appendChild(staffImg);
-//   cardBack.appendChild(staffRole);
-
-//   //content
-//   staffName.textContent = name;
-//   staffImg.src = img;
-//   staffRole.textContent = role;
-
-//   return cardBack;
-// }
