74083356390ff6bc0a749166544d06ef2aa99910
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-09-25 22:50:49 -0400

    made footer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./sassMaster/index.scss";
 
 import Head from "./head/Head";
 import Nav from "./nav/Nav";
+import Foot from "./foot/Foot";
 
 function App() {
   return (
     <div className="App">
       <Head />
       <Nav />
+      <Foot />
     </div>
   );
 }
--- src/foot/Foot.js
+++ src/foot/Foot.js
@@ -1,1 +1,11 @@
 import React from "react";
+
+function Foot() {
+  return (
+    <div className="foot">
+      <h1>FOOTER</h1>
+    </div>
+  );
+}
+
+export default Foot;
